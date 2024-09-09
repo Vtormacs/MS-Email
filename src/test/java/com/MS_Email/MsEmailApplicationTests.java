3c6262dfd495f4a9827867b9ba02d2d337836916
Vtormacs <macskids98play@hotmail.com>
2024-09-09 14:29:21 -0300

    criação

---
+++ src/test/java/com/MS_Email/MsEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MS_Email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
